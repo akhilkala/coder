2ede3bb071c0f86b7bde8b3890eb2795944d53a1
Akhil Kala <akhilkala@pop-os.localdomain>
2021-08-12 12:19:18 +0530

    cleanup

--- mobile/lib/providers/auth.dart
+++ mobile/lib/providers/auth.dart
@@ -1,14 +1,3 @@
 import 'package:flutter/foundation.dart';
 
-class User with ChangeNotifier {
-  bool isFavorite;
-
-  User({
-    this.isFavorite = false,
-  });
-
-  void toggleFavoriteStatus() {
-    // isFavorite = !isFavorite;
-    notifyListeners();
-  }
-}
+class User with ChangeNotifier {}
